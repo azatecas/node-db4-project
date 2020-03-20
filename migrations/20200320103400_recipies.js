090d542bfe38324b588784f7d4a64955bd97813b
Luis PM <luisp237@gmail.com>
2020-03-20 10:49:48 -0400

    finished table

--- migrations/20200320103400_recipies.js
+++ migrations/20200320103400_recipies.js
@@ -1,8 +1,48 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable("recipies", tbl => {
+            tbl.increments();
+            tbl.string('name', 255).notNullable();
+        })
+        .createTable("steps", tbl => {
+            tbl.increments();
+            tbl.string("steps", 255).notNullable();
+            tbl.string("details", 255).notNullable();
+            tbl.interger("recipieId").unsigned().references("id").inTable('recipies').onUpdate("CASCADE").onDelete("CASCADE")
+        })
+        .createTable("ingredients", tbl => {
+            tbl.increments();
+            tbl.string("name", 255).notNullable()
+        })
+        .createTable('ingredients_recipies', tbl => {
+            tbl.primary(['recipie_id', 'ingredient_id'])
+        
+            tbl.integer('recipie_id')
+              .unsigned()
+              .notNullable()
+              .references('recipie_id')
+              .inTable('recipie')
+              .onDelete('RESTRICT')
+              .onUpdate('CASCADE');
+              
+            tbl.integer('ingredient_id')
+              .unsigned()
+              .notNullable()
+              .references('ingredient_id')
+              .inTable('ingredients')
+              .onDelete('RESTRICT')
+              .onUpdate('CASCADE');
+        
+              tbl.string('qty')
+              .notNullable();
+          })  
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('ingredients_recipies')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('recipies')  
 };
